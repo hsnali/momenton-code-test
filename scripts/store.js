ad9eaa9404cea277d9ff1f32259e77d9f245a898
Hassan Ali <hello@hassan.so>
2017-08-13 15:30:37 +1000

    basic config files

---
+++ scripts/store.js
@@ -0,0 +1,31 @@
+export default [
+  {
+    id: 100,
+    managerId: 150,
+    name: 'Alan'
+  },
+  {
+    id: 220,
+    managerId: 100,
+    name: 'Martin'
+  },
+  {
+    id: 150,
+    name: 'Jamie'
+  },
+  {
+    id: 275,
+    managerId: 100,
+    name: 'Alex'
+  },
+  {
+    id: 400,
+    managerId: 150,
+    name: 'Steve'
+  },
+  {
+    id: 190,
+    managerId: 400,
+    name: 'David'
+  }
+]
